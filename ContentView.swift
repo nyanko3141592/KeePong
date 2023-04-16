0d49ac4ccc5f706a832fd2750c7a1b22a7f61020
nya3_neko2 <takahashinaoki521@gmail.com>
2023-04-16 16:44:37 +0900

    ドラッグでスプーンが動く

--- ContentView.swift
+++ ContentView.swift
@@ -11,17 +11,28 @@ struct ARSceneView: UIViewRepresentable {
         sceneView.delegate = context.coordinator
         sceneView.autoenablesDefaultLighting = true
         sceneView.showsStatistics = true
+
+        // Add a pan gesture recognizer to the scene view
+        let panGestureRecognizer = UIPanGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handlePanGesture(_:)))
+        sceneView.addGestureRecognizer(panGestureRecognizer)
+
         return sceneView
     }
 
     func updateUIView(_ uiView: ARSCNView, context: Context) {
         let configuration = ARWorldTrackingConfiguration()
         uiView.session.run(configuration)
+
         context.coordinator.setupSpoon(sceneView: uiView)
+        context.coordinator.setupBall(sceneView: uiView)
     }
 
     class Coordinator: NSObject, ARSCNViewDelegate {
         var sceneView: ARSceneView
+
+        // Add properties for the spoon and ball nodes
+        var spoonNode: SCNNode!
+        var ballNode: SCNNode!
 
         init(_ sceneView: ARSceneView) {
             self.sceneView = sceneView
@@ -29,27 +40,56 @@ struct ARSceneView: UIViewRepresentable {
 
         func setupSpoon(sceneView: ARSCNView) {
             let spoonScene = try! SCNScene(url: Bundle.main.url(forResource: "spoonModel", withExtension: "usdz")!)
-            let spoonNode = spoonScene.rootNode.childNodes.first!
+            spoonNode = spoonScene.rootNode.childNodes.first!
 
-            spoonNode.position = SCNVector3(0, -0.1, -0.8) // Adjust the position to your preference
-            spoonNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(0), 0, 0) // Adjust the rotation if necessary
+            spoonNode.position = SCNVector3(0, -0.1, -0.8)
+            spoonNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(0), 0, 0)
 
-            let sphereNode = createSphereNode(radius: 0.03) // Adjust the radius to fit your spoon model
-            sphereNode.position = SCNVector3(0, 0.05, 0) // Adjust the position to place the sphere on the spoon
-
-            spoonNode.addChildNode(sphereNode)
             sceneView.pointOfView?.addChildNode(spoonNode)
+
+            // Enable physics on the spoon node
+            let spoonPhysicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(node: spoonNode))
+            spoonNode.physicsBody = spoonPhysicsBody
+            spoonNode.name = "spoon"
         }
 
-        func createSphereNode(radius: CGFloat) -> SCNNode {
-            let sphere = SCNSphere(radius: radius)
-            let sphereNode = SCNNode(geometry: sphere)
+        func setupBall(sceneView: ARSCNView) {
+            let ballGeometry = SCNSphere(radius: 0.03)
+            ballGeometry.firstMaterial?.diffuse.contents = UIColor.red
+            ballNode = SCNNode(geometry: ballGeometry)
 
-            let material = SCNMaterial()
-            material.diffuse.contents = UIColor.red // Set the sphere color
-            sphere.materials = [material]
+            ballNode.position = SCNVector3(0, 0, -1)
 
-            return sphereNode
+            sceneView.scene.rootNode.addChildNode(ballNode)
+
+            // Enable physics on the ball node
+            let ballPhysicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: ballGeometry, options: nil))
+            ballNode.physicsBody = ballPhysicsBody
+            ballNode.name = "ball"
+        }
+
+        func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+            if (contact.nodeA.name == "spoon" && contact.nodeB.name == "ball") ||
+                (contact.nodeA.name == "ball" && contact.nodeB.name == "spoon")
+            {
+                // Add some force to the ball node in the direction of the spoon node's z-axis
+                let force = SCNVector3(spoonNode.presentation.worldTransform.m31,
+                                       spoonNode.presentation.worldTransform.m32,
+                                       spoonNode.presentation.worldTransform.m33)
+
+                let position = SCNVector3(contact.contactPoint.x, contact.contactPoint.y, contact.contactPoint.z)
+                ballNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
+            }
+        }
+
+        @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+            // Move the spoon node along the x and y axes based on the pan gesture
+            let translation = gestureRecognizer.translation(in: gestureRecognizer.view!)
+            var newPosition = spoonNode.position
+            newPosition.x += Float(translation.x) / 100
+            newPosition.y -= Float(translation.y) / 100
+            spoonNode.position = newPosition
+            gestureRecognizer.setTranslation(CGPoint.zero, in: gestureRecognizer.view)
         }
     }
 }
