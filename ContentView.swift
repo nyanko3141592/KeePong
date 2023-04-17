00aeb288bc04e9a019e8cf63b45478c994ef89b4
nya3_neko2 <takahashinaoki521@gmail.com>
2023-04-18 03:20:00 +0900

    add timer

--- ContentView.swift
+++ ContentView.swift
@@ -14,21 +14,33 @@ struct ARSceneView: UIViewRepresentable {
 
         // add play button
         let playbutton = UIButton(type: .system)
-        playbutton.backgroundColor = .blue
+        playbutton.backgroundColor = .systemRed
         playbutton.setTitle("Play", for: .normal)
         playbutton.setTitleColor(.white, for: .normal)
-        playbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        playbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
         playbutton.layer.cornerRadius = 15
         playbutton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 32, bottom: 12, right: 32)
         playbutton.addTarget(context.coordinator, action: #selector(Coordinator.moveBallAboveracket), for: .touchUpInside)
         playbutton.translatesAutoresizingMaskIntoConstraints = false
         sceneView.addSubview(playbutton)
 
+        // add stopwatch label
+        let stopwatchLabel = UILabel()
+        stopwatchLabel.textColor = .white
+        stopwatchLabel.font = UIFont.systemFont(ofSize: 30)
+        stopwatchLabel.textAlignment = .center
+        stopwatchLabel.translatesAutoresizingMaskIntoConstraints = false
+        sceneView.addSubview(stopwatchLabel)
+
         // Add constraints to center the button horizontally and position it at the bottom of the view
         NSLayoutConstraint.activate([
             playbutton.centerXAnchor.constraint(equalTo: sceneView.centerXAnchor),
-            playbutton.bottomAnchor.constraint(equalTo: sceneView.bottomAnchor, constant: -20)
+            playbutton.bottomAnchor.constraint(equalTo: sceneView.bottomAnchor, constant: -20),
+            stopwatchLabel.centerXAnchor.constraint(equalTo: sceneView.centerXAnchor),
+            stopwatchLabel.topAnchor.constraint(equalTo: sceneView.topAnchor, constant: 20)
         ])
+
+        context.coordinator.stopwatchLabel = stopwatchLabel // set the label as a property of the coordinator
 
         return sceneView
     }
@@ -45,6 +57,9 @@ struct ARSceneView: UIViewRepresentable {
         var racketNode: SCNNode!
         let ballRadius: CGFloat = 0.1
         var ballNode: SCNNode!
+        var stopwatchLabel: UILabel! // label to display the stopwatch time
+        var stopwatchTimer: Timer? // timer for the stopwatch
+        var elapsedSeconds: Int = 0 // number of seconds elapsed on the stopwatch
 
         init(_ sceneView: ARSceneView) {
             self.sceneView = sceneView
@@ -81,19 +96,26 @@ struct ARSceneView: UIViewRepresentable {
             ballNode.position = SCNVector3(0, 0.1, -0.3) // Set the z-coordinate of the ball to -0.03
             racketNode.addChildNode(ballNode)
             ballNode.name = "ball"
+
+            // Start or reset the stopwatch timer
+            if stopwatchTimer != nil {
+                stopwatchTimer!.invalidate()
+                elapsedSeconds = 0
+            }
+            stopwatchTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                self.elapsedSeconds += 1
+                self.updateStopwatchLabel()
+            }
+        }
+
+        func updateStopwatchLabel() {
+            let minutes = elapsedSeconds / 60
+            let seconds = elapsedSeconds % 60
+            stopwatchLabel.text = "TIME: " + String(format: "%02d:%02d", minutes, seconds)
         }
     }
 }
 
-struct ContentView: View {
-    var body: some View {
-        ARSceneView()
-            .edgesIgnoringSafeArea(.all)
-    }
-}
+struct ContentView: View { var body: some View { ARSceneView().edgesIgnoringSafeArea(.all) } }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+struct ContentView_Previews: PreviewProvider { static var previews: some View { ContentView() } }
