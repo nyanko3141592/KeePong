f864c7a0f219c5b4970f8f46e74634697a959ddf
nya3_neko2 <takahashinaoki521@gmail.com>
2023-04-18 03:15:02 +0900

    fix

--- ContentView.swift
+++ ContentView.swift
@@ -12,22 +12,22 @@ struct ARSceneView: UIViewRepresentable {
         sceneView.autoenablesDefaultLighting = true
         sceneView.showsStatistics = true
 
-        // Add a button to move the ball above the racket
-        let button = UIButton(type: .system)
-        button.backgroundColor = .blue
-        button.setTitle("Play", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.layer.cornerRadius = 15
-        button.contentEdgeInsets = UIEdgeInsets(top: 12, left: 32, bottom: 12, right: 32)
-        button.addTarget(context.coordinator, action: #selector(Coordinator.moveBallAboveracket), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        sceneView.addSubview(button)
+        // add play button
+        let playbutton = UIButton(type: .system)
+        playbutton.backgroundColor = .blue
+        playbutton.setTitle("Play", for: .normal)
+        playbutton.setTitleColor(.white, for: .normal)
+        playbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        playbutton.layer.cornerRadius = 15
+        playbutton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 32, bottom: 12, right: 32)
+        playbutton.addTarget(context.coordinator, action: #selector(Coordinator.moveBallAboveracket), for: .touchUpInside)
+        playbutton.translatesAutoresizingMaskIntoConstraints = false
+        sceneView.addSubview(playbutton)
 
         // Add constraints to center the button horizontally and position it at the bottom of the view
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: sceneView.centerXAnchor),
-            button.bottomAnchor.constraint(equalTo: sceneView.bottomAnchor, constant: -20)
+            playbutton.centerXAnchor.constraint(equalTo: sceneView.centerXAnchor),
+            playbutton.bottomAnchor.constraint(equalTo: sceneView.bottomAnchor, constant: -20)
         ])
 
         return sceneView
@@ -76,35 +76,11 @@ struct ARSceneView: UIViewRepresentable {
             ballGeometry.firstMaterial?.diffuse.contents = UIColor.white
             ballNode = SCNNode(geometry: ballGeometry)
             let ballPhysicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: ballGeometry, options: nil))
-            ballPhysicsBody.restitution = 0.8 // Set the ball's bounciness
+            ballPhysicsBody.restitution = 1 // Set the ball's bounciness
             ballNode.physicsBody = ballPhysicsBody
             ballNode.position = SCNVector3(0, 0.1, -0.3) // Set the z-coordinate of the ball to -0.03
             racketNode.addChildNode(ballNode)
             ballNode.name = "ball"
-        }
-
-        func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-            if (contact.nodeA.name == "racket" && contact.nodeB.name == "ball") ||
-                (contact.nodeA.name == "ball" && contact.nodeB.name == "racket")
-            {
-                // Add some force to the ball node in the direction of the racket node's z-axis
-                let force = SCNVector3(racketNode.presentation.worldTransform.m31,
-                                       racketNode.presentation.worldTransform.m32,
-                                       racketNode.presentation.worldTransform.m33)
-
-                let position = SCNVector3(contact.contactPoint.x, contact.contactPoint.y, contact.contactPoint.z)
-                ballNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
-            }
-        }
-
-        @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
-            // Move the racket node along the x and y axes based on the pan gesture
-            let translation = gestureRecognizer.translation(in: gestureRecognizer.view!)
-            var newPosition = racketNode.position
-            newPosition.x += Float(translation.x) / 100
-            newPosition.y -= Float(translation.y) / 100
-            racketNode.position = newPosition
-            gestureRecognizer.setTranslation(CGPoint.zero, in: gestureRecognizer.view)
         }
     }
 }
